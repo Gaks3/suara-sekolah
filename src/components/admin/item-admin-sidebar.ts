3c311076c0758b793f77fded6f3ecc403e33eb20
Gaks3 <dwi66116@gmail.com>
2025-04-24 14:23:48 +0700

    feat: create admins dashboard sidebar component

---
+++ src/components/admin/item-admin-sidebar.ts
@@ -0,0 +1,14 @@
+import { HomeIcon, UsersRoundIcon } from "lucide-react"
+
+export const items = [
+  {
+    title: "Home",
+    url: "/admin",
+    icon: HomeIcon,
+  },
+  {
+    title: "Users",
+    url: "/admin/users",
+    icon: UsersRoundIcon,
+  },
+]
