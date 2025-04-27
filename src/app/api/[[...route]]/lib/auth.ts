9e6a10ee29a145d43e4bf99c91733db9b1502d4e
Gaks3 <dwi66116@gmail.com>
2025-04-27 14:08:23 +0700

    feat: create new role and permissions

--- src/app/api/[[...route]]/lib/auth.ts
+++ src/app/api/[[...route]]/lib/auth.ts
@@ -1,8 +1,11 @@
-import { betterAuth } from "better-auth"
-import { prismaAdapter } from "better-auth/adapters/prisma"
-import { admin, openAPI } from "better-auth/plugins"
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { admin as adminPlugin, openAPI } from "better-auth/plugins";
 
-import db from "./db"
+import { User, UserRoleSchema } from "../../../../../prisma/generated/zod";
+import db from "./db";
+import { ac, admin, guru, karyawan, siswa } from "./permissions";
+import { UserRole } from "@prisma/client";
 
 export const auth = betterAuth({
   database: prismaAdapter(db, { provider: "mysql" }),
@@ -16,20 +19,65 @@ export const auth = betterAuth({
     additionalFields: {
       role: {
         type: "string",
+        validator: {
+          input: UserRoleSchema.refine((value) => value === "admin", {
+            message: "Forbidden",
+          }),
+        },
+        input: true,
       },
       phone: {
         type: "string",
       },
       nis: {
         type: "string",
+        required: false,
       },
       nip: {
         type: "string",
+        required: false,
       },
     },
     deleteUser: {
       enabled: true,
     },
   },
-  plugins: [openAPI(), admin()],
-})
+  databaseHooks: {
+    user: {
+      create: {
+        after: async (user) => {
+          const formattedUser = user as User;
+
+          if (formattedUser.nis)
+            await db.user.update({
+              where: { id: formattedUser.id },
+              data: { role: "siswa" },
+            });
+          else if (formattedUser.nip)
+            await db.user.update({
+              where: { id: formattedUser.id },
+              data: { role: "guru" },
+            });
+          else if (!formattedUser.nis && !formattedUser.nip)
+            await db.user.update({
+              where: { id: formattedUser.id },
+              data: { role: "karyawan" },
+            });
+        },
+      },
+    },
+  },
+  plugins: [
+    openAPI(),
+    adminPlugin({
+      ac,
+      roles: {
+        siswa,
+        guru,
+        karyawan,
+        admin,
+      },
+      defaultRole: UserRole.siswa,
+    }),
+  ],
+});
